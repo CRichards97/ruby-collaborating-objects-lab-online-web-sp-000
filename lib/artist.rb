c03230698a7b36f99c278c61d5070c1f90c4848c
Clint Richards <richardsclint17@gmail.com>
2020-12-31 03:28:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,7 @@ class Artist
     self.new(name).tap {|artist| artist.save}
   end
   def save
-  @@all << self unless @@all.include?(self) 
+  @@all << self unless @@all.include?(self)
 end
   def print_songs
     songs.each {|song| puts song.name}
